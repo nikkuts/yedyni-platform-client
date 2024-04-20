5ecdf48531524847d3d4767163a62dd297891664
nikkuts <nickkuts888@gmail.com>
2024-04-20 22:53:42 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter 
-          // basename={BASE_NAME}
+          basename={BASE_NAME}
         >
           <App />
         </BrowserRouter>
